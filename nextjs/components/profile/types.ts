490776458fad0e476db75a4fbc1d5f2fd3f71757
Caroline Ennis <caroline@groupthink.com>
2026-02-01 17:11:56 +1300

    let user edit location

--- nextjs/components/profile/types.ts
+++ nextjs/components/profile/types.ts
@@ -1,6 +1,7 @@
 import CameraAltIcon from '@mui/icons-material/CameraAlt';
 import PersonIcon from '@mui/icons-material/Person';
 import LocationOnIcon from '@mui/icons-material/LocationOn';
+import HomeIcon from '@mui/icons-material/Home';
 import StarIcon from '@mui/icons-material/Star';
 import AccessTimeIcon from '@mui/icons-material/AccessTime';
 import EventIcon from '@mui/icons-material/Event';
@@ -11,6 +12,7 @@ import PublicIcon from '@mui/icons-material/Public';
 export const navItems = [
   { id: 'photo', label: 'Photo', icon: CameraAltIcon },
   { id: 'about', label: 'About', icon: PersonIcon },
+  { id: 'location', label: 'Location', icon: HomeIcon },
   { id: 'studio', label: 'Studio', icon: LocationOnIcon },
   { id: 'styles', label: 'Styles', icon: StarIcon },
   { id: 'hours', label: 'Hours', icon: AccessTimeIcon },
