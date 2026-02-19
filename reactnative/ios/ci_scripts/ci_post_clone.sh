9e113ce5dbc8500eed584b62067610e4f899a782
Caroline Ennis <caroline@groupthink.com>
2026-02-19 20:03:15 +1300

    updating ci_post_clone.sh

--- reactnative/ios/ci_scripts/ci_post_clone.sh
+++ reactnative/ios/ci_scripts/ci_post_clone.sh
@@ -1,7 +1,16 @@
 #!/bin/sh
 set -e
 
+echo "Installing Node.js..."
+brew install node
+export NODE_BINARY=$(which node)
+
+echo "Installing npm dependencies..."
+cd "$CI_PRIMARY_REPOSITORY_PATH/reactnative"
+npm install
+
 echo "Installing CocoaPods dependencies..."
-cd "$CI_PRIMARY_REPOSITORY_PATH/inked-in-www/reactnative/ios"
+cd ios
 pod install
-echo "CocoaPods installation complete."
+
+echo "Build setup complete."
