9477dae818e0a60b97953411ea4dda72ed9db33a
Caroline Ennis <caroline@groupthink.com>
2026-03-01 11:19:53 +1300

    fixing build issues

--- reactnative/ios/ci_scripts/ci_post_clone.sh
+++ reactnative/ios/ci_scripts/ci_post_clone.sh
@@ -12,6 +12,10 @@ echo "Installing npm dependencies..."
 cd "$CI_PRIMARY_REPOSITORY_PATH/reactnative"
 npm install
 
+echo "Setting up CocoaPods repo..."
+pod repo remove trunk 2>/dev/null || true
+pod repo add-cdn trunk https://cdn.cocoapods.org/
+
 echo "Installing CocoaPods dependencies..."
 cd ios
 pod install
